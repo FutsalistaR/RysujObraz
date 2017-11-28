b8e9713985c1d6cbe9c96400cca1c98245de7ca8
Baciu555 <wojtekbac95@gmail.com>
2017-11-29 00:43:45 +0100

    point z field added

--- src/rysujobraz/Point.java
+++ src/rysujobraz/Point.java
@@ -13,23 +13,33 @@ public class Point {
     
     private int x;
     private int y;
+    private int z;
+    
+	public Point(int x, int y, int z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}	
+	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
 
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public int getZ() {
+		return z;
+	}
 	@Override
 	public String toString() {
-		return "Point [x=" + x + ", y=" + y + "]";
+		return "Point [x=" + x + ", y=" + y + ", z=" + z + "]";
 	}
+
+    
     
 }
